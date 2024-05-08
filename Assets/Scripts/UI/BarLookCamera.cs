7f48984ae78b60a8621750910d162abdd58acc6a
DatNguyen602 <ngoclanhlethi2@gmail.com>
2024-05-08 12:55:30 +0700

    Check: recommit

---
+++ Assets/Scripts/UI/BarLookCamera.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarLookCamera : MonoBehaviour
+{
+    private Camera _camera;
+
+    private void Awake() {
+        _camera = Camera.main;
+    }
+
+    private void LateUpdate() {
+        transform.rotation = Quaternion.LookRotation(_camera.transform.forward);
+    }
+}
